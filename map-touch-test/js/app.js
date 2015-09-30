534fa0842fe38842db36bbb96b6fd0e775f2c52e
durbonix <robert.durbin@gmail.com>
2015-09-30 17:53:12 -0400

    added in error handling

--- map-touch-test/js/app.js
+++ map-touch-test/js/app.js
@@ -1,34 +1,32 @@
-function initMap(){
+function initMap() {
     // if HTML DOM Element that contains the map is found...
-alert("ehll");
-if (document.getElementById('map-canvas')){
- 
-    // Coordinates to center the map
-    var myLatlng = new google.maps.LatLng(52.525595,13.393085);
- 
-    // Other options for the map, pretty much selfexplanatory
-    var mapOptions = {
-        zoom: 14,
-        center: myLatlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
- 
-    // Attach a map to the DOM Element, with the defined settings
-    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-  
-  google.maps.event.addListener(map, "click", function (e) {
+    if (document.getElementById('map-canvas')) {
+        console.log("here");
+        // Coordinates to center the map
+        var myLatlng = new google.maps.LatLng(52.525595, 13.393085);
 
-    //lat and lng is available in e object
-    var latLng = e.latLng;
-    var position = new google.maps.LatLng(e.latLng.lat(), e.latLng.lng());
-                                          
-    var marker = new google.maps.Marker({
-            position: position,
-            map: map,
-            title: 'hello'
+        // Other options for the map, pretty much selfexplanatory
+        var mapOptions = {
+            zoom: 14,
+            center: myLatlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        // Attach a map to the DOM Element, with the defined settings
+        var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+        alert("hell");
+        google.maps.event.addListener(map, "click", function(e) {
+
+            //lat and lng is available in e object
+            var latLng = e.latLng;
+            var position = new google.maps.LatLng(e.latLng.lat(), e.latLng.lng());
+
+            var marker = new google.maps.Marker({
+                position: position,
+                map: map,
+                title: 'hello'
+            });
         });
-});
 
+    }
 }
-}
-
