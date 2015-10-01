8d0466e033ceea9f78e10023cb853a644ba4dc0d
durbonix <robert.durbin@gmail.com>
2015-10-01 10:11:43 -0400

    removed redundant code

--- map-touch-test/js/app.js
+++ map-touch-test/js/app.js
@@ -2,7 +2,6 @@ function initMap() {
     /*
     // if HTML DOM Element that contains the map is found...
     if (document.getElementById('map-canvas')) {
-        console.log("here");
         // Coordinates to center the map
         var myLatlng = new google.maps.LatLng(52.525595, 13.393085);
 
@@ -15,22 +14,6 @@ function initMap() {
 
         // Attach a map to the DOM Element, with the defined settings
         var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-        alert("hell");
-        google.maps.event.addListener(map, "click", function(e) {
-
-            //lat and lng is available in e object
-            var latLng = e.latLng;
-            var position = new google.maps.LatLng(e.latLng.lat(), e.latLng.lng());
-
-            var marker = new google.maps.Marker({
-                position: position,
-                map: map,
-                title: 'hello'
-            });
-        });
-        
-        Three Chopt, VA
-        37.630458, -77.547904
 
     }*/
     console.log("added in listener");
